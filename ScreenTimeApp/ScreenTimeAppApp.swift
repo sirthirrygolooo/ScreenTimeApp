27bee491e48fd5f753b27b7c8c1f80d62f2aac3b
MAC IOS <magico7500@gmail.com>
2025-03-11 08:27:18 +0100

    Initial Commit

---
+++ ScreenTimeApp/ScreenTimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeAppApp.swift
+//  ScreenTimeApp
+//
+//  Created by froehly jean-baptiste on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
